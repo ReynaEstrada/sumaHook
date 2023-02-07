8aa3c2967c4ed312e4ecf2a92369fd81e62d4eaf
ReynaEstrada <iscsoftware384@gmai.com>
2023-02-06 21:14:48 -0600

    modifiedNamesVariables

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [suma, setSuma] = useState(0);
   const Increment10 = () => {
-    setCount(prevCount => prevCount+10);
+    setSuma(sumNum => sumNum +10);
   };
   const Increment20 = () => {
-    setCount(prevCount => prevCount +20);
+    setSuma(sumNum => sumNum +20);
   };
   const Increment30 = () => {
-    setCount(prevCount => prevCount +30);
+    setSuma(sumNum => sumNum +30);
   };
   const Increment40 = () => {
-    setCount(prevCount => prevCount +40);
+    setSuma(sumNum => sumNum +40);
   };
   const Increment50 = () => {
-    setCount(prevCount => prevCount +50);
+    setSuma(sumNum => sumNum +50);
   };
 
   return (
     <div className="App">
-      <div>The result is:  {count}</div>
+      <div>The result is:  {suma}</div>
       <div className="card">
         <button onClick={Increment10}>10</button>
         <button onClick={Increment20}>20</button>
@@ -29,7 +29,7 @@ function App() {
         <button onClick={Increment40}>40</button>
         <button onClick={Increment50}>50</button>
       </div>
-      <button onClick={() => setCount(0)}>
+      <button onClick={() => setSuma(0)}>
         Reset
       </button>
     </div>
